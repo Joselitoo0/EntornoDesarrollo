6c347ef77ce27a42c37570ac5f2f0d424e797149
José Ángel Pérez Lóp <Jos ngel Prez Lp@192.168.56.1>
2019-03-29 10:01:08 +0100

    commit clase principal

--- EjercicioEntorno/src/ejercicioEntorno/Principal.java
+++ EjercicioEntorno/src/ejercicioEntorno/Principal.java
@@ -1,10 +1,14 @@
 package ejercicioEntorno;
 
+
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
-
+		Vista Vista = new Vista();
+		Modelo Modelo = new Modelo();
+		new Controlador(Vista, Modelo);
 	}
 
 }
